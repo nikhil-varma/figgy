515bb68e5befe090668e809470dd5b56245abaa1
Nikhil Varma <nikhilvarma892@gmail.com>
2020-12-08 23:03:42 +0530

    Feat: Add light and dark theme

---
+++ src/themes/theme.js
@@ -0,0 +1,14 @@
+const lightTheme = {
+  colors: {
+    background: 'white',
+    textColor: 'black',
+  },
+};
+const darkTheme = {
+  colors: {
+    background: 'black',
+    textColor: 'white',
+  },
+};
+export {lightTheme as light};
+export {darkTheme as dark};
